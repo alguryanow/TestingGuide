c3c10edd3f179e31d19639b665e9f24bcfda982a
Aleksei Gurianov <avgurianov@itmo.ru>
2024-03-06 11:30:43 +0300

    Рефакторинг: выделил общую часть в отдельный подключаемый файл

--- tests/integr_test2.cpp
+++ tests/integr_test2.cpp
@@ -1,44 +1,4 @@
-#include "../src/Server.h"
-#include "../src/Client.h"
-
-#include <systemc>
-using namespace sc_core;
-
-#define DOCTEST_CONFIG_IMPLEMENT
-#include "../../doctest.h"
-
-int sc_main(int argc, char* argv[]) { return 0;} // заглушка для возможности линковки с systemc
-
-int main(int argc, char* argv[])
-{
-    doctest::Context context;
-
-    int res = context.run();
-
-    return res;
-}
-
-
-TEST_CASE("test1")
-{
-    //-- Сервер и Клиент
-    Server server("SERVER");
-    Client client("CLIENT");
-
-    //-- через что они будут обмениваться
-    sc_fifo<int> client_to_server;
-    sc_fifo<int> server_to_client1;
-    sc_fifo<int> server_to_client2;
-
-    //-- связываю Клиента с Сервером
-    server.data_in(client_to_server);
-    server.data_out1(server_to_client1);
-    server.data_out2(server_to_client2);
-
-    client.data_in1(server_to_client1);
-    client.data_in2(server_to_client2);
-    client.data_out(client_to_server);
-
+#include "integr_test_common.inc"
 
     //-- задаю входные данные для тестирования
     client.m_out_value = -10;
